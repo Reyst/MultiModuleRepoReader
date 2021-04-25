8eb72b113fffba054217cceaf4234b44d5b26e8f
Reyst <oleksenko.pm@gmail.com>
2021-04-25 16:00:22 +0300

    add some fixes

--- app/src/main/java/reyst/gsihome/research/mmr/ui/MainActivity.kt
+++ app/src/main/java/reyst/gsihome/research/mmr/ui/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import reyst.gsihome.research.mmr.R
@@ -19,15 +17,13 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    private val viewModel: SearchResultViewModel by lazy {
-        ViewModelProviders.of(this, viewModelFactory)[SearchResultViewModel::class.java]
-    }
+    private val viewModel: SearchResultViewModel by lazy { ViewModelProvider(this, viewModelFactory)[SearchResultViewModel::class.java] }
 
     private val adapter = RepoAdapter()
 
-    private val etName: EditText by lazy { findViewById<EditText>(R.id.name) }
-    private val btn: Button by lazy { findViewById<Button>(R.id.button) }
-    private val list: RecyclerView by lazy { findViewById<RecyclerView>(R.id.list) }
+    private val etName: EditText by lazy { findViewById(R.id.name) }
+    private val btn: Button by lazy { findViewById(R.id.button) }
+    private val list: RecyclerView by lazy { findViewById(R.id.list) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,9 +38,6 @@ class MainActivity : AppCompatActivity() {
         list.adapter = adapter
         list.layoutManager = LinearLayoutManager(this)
 
-        viewModel.repoList.observe(
-            this,
-            Observer<List<reyst.gsihome.research.core.Repo>> { adapter.update(it) }
-        )
+        viewModel.repoList.observe(this, adapter::update)
     }
 }
--- app/src/main/java/reyst/gsihome/research/mmr/ui/SearchResultViewModel.kt
+++ app/src/main/java/reyst/gsihome/research/mmr/ui/SearchResultViewModel.kt
@@ -3,6 +3,7 @@ package reyst.gsihome.research.mmr.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import reyst.gsihome.research.core.GitHubRepoRepository
 import reyst.gsihome.research.core.Repo
@@ -17,32 +18,24 @@ class SearchResultViewModel @Inject constructor(
     val repoList: LiveData<List<Repo>>
     get() = _repoList
 
-    private val viewModelJob = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
-
     fun searchRepos1(name: String) {
-        uiScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(Dispatchers.IO) {
             val data = repository.getRepoListByUsername(name)
             _repoList.postValue(data)
         }
     }
 
     fun searchRepos2(name: String) {
-        uiScope.launch {
+        viewModelScope.launch {
             val data = async(Dispatchers.IO) { repository.getRepoListByUsername(name) }
             _repoList.value = data.await()
         }
     }
 
     fun searchRepos3(name: String) {
-        uiScope.launch {
+        viewModelScope.launch {
             val data = withContext(Dispatchers.IO) { repository.getRepoListByUsername(name) }
             _repoList.value = data
         }
-    }
-
-    override fun onCleared() {
-        viewModelJob.cancel()
-        super.onCleared()
     }
 }
